4d0727f0271e77c4c39b7250ae8a663e85559071
Adam Matthews <ammatthews2004@gmail.com>
2025-01-06 13:07:33 +0000

    adding some pictures

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -58,14 +58,14 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-            <Image
-              src="/logo.png"
+          <div className="rounded-full bg-[url(/me.jpg)] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] bg-cover bg-center relative">
+            {/*<Image
+              src="/me.jpg"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
               height={300}
-            />
+            />*/}
           </div>
         </motion.div>
       </div>
